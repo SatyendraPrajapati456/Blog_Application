14a856ffe939371fd1b36988090c99e013080530
SatyendraPrajapati456 <sp5267062@gmail.com>
2026-03-13 00:48:06 +0530

    Delete blog-app directory

--- blog-app/routes/commentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router({ mergeParams: true });
-const { postComment, deleteComment } = require('../controllers/commentController');
-const { requireAuth, requireCommentAuthor } = require('../middleware/authMiddleware');
-
-router.post('/', requireAuth, postComment);
-router.delete('/:commentId', requireAuth, requireCommentAuthor, deleteComment);
-
-module.exports = router;
